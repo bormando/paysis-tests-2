f3ef28ce9da2ed66197c75f890fd922e79a9ff0b
Dmitrii Bormotov <squier7@gmail.com>
2022-10-11 08:28:07 +0400

    Add before hook for authentication tests Divide assertions in authentication tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,25 +1,40 @@
 import supertest from 'supertest'
 import {expect} from 'chai'
-import 'dotenv/config'
 
 describe('Authentication', function () {
-  it('Log in with valid credentials', function () {
-    supertest(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-      .end((err, res) => {
-        expect(res.statusCode).to.eq(200)
-        expect(res.body.token).not.to.be.undefined
-      })
+  describe('Log in with valid credentials', function () {
+    let response
+
+    before(async function () {
+      response = await supertest(process.env.BASE_URL)
+        .post('/auth')
+        .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+    })
+
+    it('Response status code is 200', function () {
+      expect(response.statusCode).to.eq(200)
+    })
+
+    it('Response body contains token', function () {
+      expect(response.body.token).not.to.be.undefined
+    })
   })
 
-  it('Log in with incorrect credentials', function () {
-    supertest(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: 'invalid', password: 'invalid'})
-      .end((err, res) => {
-        expect(res.statusCode).to.eq(404)
-        expect(res.body.message).to.eq('Wrong login or password.')
-      })
+  describe('Log in with incorrect credentials', function () {
+    let response
+
+    before(async function () {
+      response = await supertest(process.env.BASE_URL)
+        .post('/auth')
+        .send({login: 'invalid', password: 'invalid'})
+    })
+
+    it('Response status code is 404', function () {
+      expect(response.statusCode).to.eq(404)
+    })
+
+    it('Response body contains error message', function () {
+      expect(response.body.message).to.eq('Wrong login or password.')
+    })
   })
 })
