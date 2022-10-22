dac092e06b395c7bacc1694713b4ae1e19c0449e
Dmitrii Bormotov <squier7@gmail.com>
2022-10-22 07:54:12 +0400

    Add Transaction test suite Add Create scenario with valid data

---
+++ tests/transaction.spec.js
@@ -0,0 +1,59 @@
+import UserHelper from '../helpers/user.helper'
+import TransactionHelper from '../helpers/transaction.helper'
+import {expect} from "chai";
+
+describe('Transaction', function () {
+  const defaultBalance = 1000
+
+  describe('Create', function () {
+    describe.only('With valid data', function () {
+      const userHelper = new UserHelper()
+      const transactionHelper = new TransactionHelper()
+      const amount = 100
+      let createTransactionResponse
+      let userIdFrom
+      let userIdTo
+
+      before(async function () {
+        userIdFrom = (await userHelper.create()).body.id
+        userIdTo = (await userHelper.create()).body.id
+        createTransactionResponse = await transactionHelper.create(userIdFrom, userIdTo, amount)
+      })
+
+      after(async function () {
+        await userHelper.delete(userIdFrom)
+        await userHelper.delete(userIdTo)
+      })
+
+      it('Create Transaction response status code is 200', function () {
+        expect(createTransactionResponse.statusCode).to.eq(200)
+      })
+
+      it('Create Transaction response body contains Transaction ID', function () {
+        expect(createTransactionResponse.body.id).to.be.a('string')
+      })
+
+      it('Create Transaction response body contains User ID of sender', function () {
+        expect(createTransactionResponse.body.from).to.eq(userIdFrom)
+      })
+
+      it('Create Transaction response body contains User ID of receiver', function () {
+        expect(createTransactionResponse.body.to).to.eq(userIdTo)
+      })
+
+      it('Create Transaction response body contains entered amount', function () {
+        expect(createTransactionResponse.body.amount).to.eq(amount)
+      })
+
+      it('Sender\'s balance had decreased by 100', async function () {
+        const response = await userHelper.get(userIdFrom)
+        await expect(response.body.amount).to.eq(defaultBalance-amount)
+      })
+
+      it('Receiver\'s balance has increased by 100', async function () {
+        const response = await userHelper.get(userIdTo)
+        await expect(response.body.amount).to.eq(defaultBalance+amount)
+      })
+    })
+  })
+})
